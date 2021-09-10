2e44d71135a85dcca8e6e8afe74801514c6c9cfc
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-10 10:45:51 +0300

    Created 'LoginPage' class

---
+++ Tests/Pages/LoginPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Pages
+{
+    public class LoginPage
+    {
+        private string urlLogin = "http://eaapp.somee.com/Account/Login";
+
+        private IWebDriver Driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+    }
+}
