2ea77967a43599d0842495be9dff48888e0e9339
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-10 11:48:52 +0300

    Added 'CreatePage' class with 'OpenCreatePage' method

---
+++ Tests/TestCreateNewEmployee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using Tests.Pages;
+
+namespace Tests
+{
+    public class TestCreateNewEmployee:Drivers
+    {
+        public string urlHome = "http://eaapp.somee.com/";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            ChooseDriver(Browsers.Chrome);
+            Navigate(urlHome);
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.IfLoggedIn();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Driver.Close();
+        }
+
+        [Test]
+        public void SuccessCreateNewEmployee()
+        {
+            CreatePage createPage = new CreatePage(Driver);
+            createPage.OpenCreatePage();
+        }
+    }
+}
