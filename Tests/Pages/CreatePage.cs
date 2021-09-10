d55d3a2605fa012315ab9a9760156fbed73b43ca
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-10 11:34:57 +0300

    Created 'CreatePage' class with 'OpenCreatePage' method

---
+++ Tests/Pages/CreatePage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Pages
+{
+    public class CreatePage
+    {
+        private IWebDriver Driver { get; set; }
+
+        public CreatePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        private IWebElement CreateNewBtn => Driver.FindElement(By.XPath("//a[text()='Create New']"));
+        private IWebElement CreateLnk => Driver.FindElement(By.XPath("//input[@type='submit']"));
+
+        public CreatePage OpenCreatePage()
+        {
+            CreateNewBtn.Click();
+            return new CreatePage(Driver);
+        }
+    }
+}
