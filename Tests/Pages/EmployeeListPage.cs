ab262d2e404898c23b43119eeeb2da8bc912a8c6
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-10 11:53:01 +0300

    Created 'EmployeeListPage' class with 'EmployeePageNavigate' method

---
+++ Tests/Pages/EmployeeListPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Pages
+{
+    public class EmployeeListPage
+    {
+        private IWebDriver Driver;
+        public EmployeeListPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        private IWebElement EmployeeList => Driver.FindElement(By.LinkText("Employee List"));
+        private IWebElement SearchInput => Driver.FindElement(By.XPath("//input[@type='submit']"));
+        private IWebElement CreateNewBtn => Driver.FindElement(By.XPath("//a[text()='Create New']"));
+
+        public EmployeeListPage EmployeePageNavigate()
+        {
+            EmployeeList.Click();
+            return new EmployeeListPage(Driver);
+        }
+    }
+}
