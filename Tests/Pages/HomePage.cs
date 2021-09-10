417b618cc22fce56d3fa9ae70b74fc947983454f
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-10 10:43:33 +0300

    Created 'HomePage' class with locator for 'Login' link

---
+++ Tests/Pages/HomePage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests.Pages
+{
+    public class HomePage
+    {
+        private IWebDriver Driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        private IWebElement loginLink => Driver.FindElement(By.Id("loginLink"));
+        
+
+        public void NavigateToLoginPage()
+        {
+
+        }
+    }
+}
