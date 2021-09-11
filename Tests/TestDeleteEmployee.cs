19d97f5c4cf561495d547ab9519c12dc852e22aa
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-11 06:11:07 +0300

    Create setup and teardown for test 'TestDeleteEmployee'

---
+++ Tests/TestDeleteEmployee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Tests.Pages;
+
+namespace Tests
+{
+    public class TestDeleteEmployee:Drivers
+    {
+        private string urlHome = "http://eaapp.somee.com/";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            ChooseDriver(Browsers.Chrome);
+            Navigate(urlHome);
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.IfLoggedOff("admin", "password");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Driver.Close();
+        }
+
+        [Test]
+        public void DeleteUser()
+        {
+
+        }
+    }
+}
