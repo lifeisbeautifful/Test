9a093c537028100b4e07ae59e2b0a92adeb9692f
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-11 07:08:00 +0300

    Added 'TestSearch' test

---
+++ Tests/TestSearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Tests.Pages;
+
+namespace Tests
+{
+    public class TestSearch:Drivers
+    {
+        public string urlHome = "http://eaapp.somee.com/";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            ChooseDriver(Browsers.Chrome);
+            Navigate(urlHome);
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.IfLoggedOff("admin", "password");
+
+            EmployeeListPage employeeListPage = new EmployeeListPage(Driver);
+            employeeListPage.EmployeePageNavigate();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        { 
+            Driver.Close();
+        }
+
+        [Test]
+        public void PerformSearch()
+        {
+            EmployeeListPage employeeListPage = new EmployeeListPage(Driver);
+            var employees = employeeListPage.SearchEmployee("Test", "mult");
+            bool result = employeeListPage.CheckFoundEmpData(employees, "mult", "Test");
+            Assert.That(result, Is.True, "Not all found users follow search criteria");
+        }
+            
+    }
+}
