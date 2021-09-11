77af04de70a202cffaece566767dfaa8e5685762
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-11 06:55:24 +0300

    Added 'TestEditEmployee'

---
+++ Tests/TestEditEmployee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Tests.Pages;
+
+namespace Tests
+{
+    public class TestEditEmployee:Drivers
+    {
+        public string urlHome = "http://eaapp.somee.com/";
+        string[] employeeData = { "Oksana", "4000", "2", "4", "a@mailforspam.com" };
+        string[] employeeEditedData = { "Name", "3000", "1", "3", "a@mailforspam.com" };
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            ChooseDriver(Browsers.Chrome);
+            Navigate(urlHome);
+
+            LoginPage loginPage = new LoginPage(Driver);
+            loginPage.IfLoggedOff("admin", "password");
+
+            EmployeeListPage employeeListPage = new EmployeeListPage(Driver);
+            employeeListPage.CreateUser(employeeEditedData, employeeData);
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            DeletePage deletePage = new DeletePage(Driver);
+            deletePage.DeleteEmployee(employeeEditedData[0]);
+            Driver.Close();
+        }
+
+        [Test]
+        public void EditEmployeeData()
+        {
+            CreatePage createPage = new CreatePage(Driver);
+            EmployeeListPage employeeListPage = new EmployeeListPage(Driver);
+
+            employeeListPage.EmployeePageNavigate();
+            employeeListPage.SearchEmployee(employeeData[0], "single");
+            employeeListPage.TestEditLink();
+            createPage.CreateEditEmployee(employeeEditedData, employeeData);
+            Assert.IsTrue(employeeListPage.IsAt, "User is not navigated back to 'Employee List' page from 'Edit' page");
+
+            var foundEditedEmployee = employeeListPage.SearchEmployee(employeeEditedData[0], "single");
+            Assert.IsTrue(employeeListPage.CheckFoundEmpData(foundEditedEmployee, employeeEditedData));
+        }
+    }
+}
