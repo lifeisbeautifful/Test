ecf1c28f0679ee99926d6229a922b37783cbe01c
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-12 15:40:42 +0300

    Created 'EditPage' class

---
+++ Tests/Pages/EditPage.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests.Pages
+{
+    public class EditPage
+    {
+        private IWebDriver Driver { get; set; }
+        public EditPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public void EditEmployee(string[] newUserData, params string[] oldUserData)
+        {
+            List<IWebElement> inputs = Driver.FindElements(By.TagName("input")).ToList();
+            int i = 0;
+
+            for (int j = 0; j < inputs.Count; j++)
+            {
+                if (i < 5)
+                {
+                    if (inputs[j].GetAttribute("value") == oldUserData[i])
+                    {
+                        inputs[j].Clear();
+                        inputs[j].SendKeys(newUserData[i]);
+                        i++;
+                    }
+                }
+
+                if (j == inputs.Count - 1)
+                {
+                    inputs[j].Click();
+                }
+            }
+        }
+        }
+}
