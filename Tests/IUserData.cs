30762565cb8307f4f1a14fdb87403f34a6e71b6b
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-18 07:48:47 +0300

    Added properties to 'IUserData

--- Tests/IUserData.cs
+++ Tests/IUserData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,13 @@ namespace Tests
 {
     public interface IUserData
     {
+        public IWebElement UserName { get; set; }
+        public IWebElement Password { get; set; }
+        public IWebElement Name { get; set; }
+        public IWebElement Salary { get; set; }
+        public IWebElement DurationWorked { get; set; }
+        public IWebElement Grade { get; set; }
+        public IWebElement Email { get; set; }
         public void SetOrChangeUserData(string[] newUserData, params string[] oldUserData);
     }
 }
