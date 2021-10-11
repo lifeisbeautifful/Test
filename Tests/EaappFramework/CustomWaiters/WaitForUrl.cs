912eb54bdff03eb6e8c8081ce402b472861f7adb
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-10-11 12:01:03 +0300

    Added WaitForUrl class and implement custom wait in EditEmployeeTest

---
+++ Tests/EaappFramework/CustomWaiters/WaitForUrl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Tests.EaappFramework.CustomWaiters
+{
+    public static class WaitForUrl
+    {
+        private const int DefaultTimeoutInSeconds = 60;
+        public static void WaitForPageUrl(bool condition)
+        {
+            Stopwatch timer = Stopwatch.StartNew();
+
+            do
+            {
+                if (condition)
+                {
+                    return;
+                }
+            } while (timer.Elapsed.TotalSeconds < DefaultTimeoutInSeconds);
+
+            throw new TimeoutException();
+        }
+    }
+}
---
+++ Tests/EaappUI/Urls/EAAPPUrls.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Tests.Urls
+{
+    public static class EAAPPUrls
+    {
+        public static string urlHome = "http://eaapp.somee.com/";
+        public static string urlCreatePage = "http://eaapp.somee.com/Employee/Create";
+        public static string urlEmployeeListPage = "http://eaapp.somee.com/Employee";
+    }
+}
